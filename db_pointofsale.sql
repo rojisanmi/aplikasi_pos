cb39134838c31f1ff5aa06f778f05f889cd7393a
rojisanmi <rojiihsan21@gmail.com>
2024-10-24 20:19:08 +0700

    File SQL
    Berikut adalah file SQL untuk database-nya

---
+++ db_pointofsale.sql
@@ -0,0 +1,399 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2024 at 03:18 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pointofsale`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_produk`
+--
+
+CREATE TABLE `kategori_produk` (
+  `id` int(11) NOT NULL,
+  `kategori` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kategori_produk`
+--
+
+INSERT INTO `kategori_produk` (`id`, `kategori`) VALUES
+(1, 'Makanan'),
+(2, 'Minuman'),
+(3, 'Obat'),
+(4, 'Buku'),
+(5, 'Ramuan Herbal'),
+(6, 'Pemain');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_kelamin` set('Pria','Wanita','Lainya') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telepon` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id`, `nama`, `jenis_kelamin`, `alamat`, `telepon`) VALUES
+(5, 'Roji Ihsan Azmi', 'Pria', 'Bandung', '0800'),
+(6, 'Revan Naufal Arifin', 'Wanita', 'Bogor', '121212');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo_user` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`id`, `username`, `password`, `nama`, `role`, `photo_user`) VALUES
+(1, 'admin', '$2y$10$/I7laWi1mlNFxYSv54EUPOH8MuZhmRWxhE.LaddTK9TSmVe.IHP2C', 'Admin', '1', 'avatar51.png'),
+(2, 'ibrahimalanshor', '$2y$10$5thNuizSyAdrGXC9A/WYd.StNiSRUy0eBZJ401hGBfUpwGINu9kyG', 'Ibrahim Al Anshor', '2', 'user2-160x160.jpg'),
+(3, 'ihsan', '$2y$10$DQGkSZrPhrUYcPLU0rqtJ.BgQnmL8UmgHBFJdpFronKBj8zLOosae', 'Roji Ihsan Azmi', '2', 'Screenshot_2022-04-03_152627.png'),
+(8, 'roji_sanmi', '$2y$10$wCFPCzsLhEw3ArhLbbBBQOBAKRYLZj/D6iRB441OvDnYasmHMuTaC', 'Roji Ihsan Azmah', '2', 'POSTER_(1080_×_1080_px).png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` int(11) NOT NULL,
+  `barcode` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_produk` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kategori` int(11) NOT NULL,
+  `satuan` int(11) NOT NULL,
+  `harga` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stok` int(11) NOT NULL,
+  `terjual` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id`, `barcode`, `nama_produk`, `kategori`, `satuan`, `harga`, `stok`, `terjual`) VALUES
+(1, 'THKTK-ULTRAJAYA', 'Teh Kotak', 2, 5, '25000', 0, ''),
+(2, 'MKNCHUBA', 'Snack Chuba', 1, 5, '50000', 0, ''),
+(3, 'BKTLS-SIDU', 'Buku Tulis', 4, 5, '24000', 80, '9'),
+(4, 'uuuaa', 'Teh KOtak uuaa', 2, 5, '50000', 37, '1'),
+(5, 'OPO', 'Buku Tulis Opo', 4, 1, '24000', 9, '1'),
+(6, 'SNKARIES', 'Snack aRIES', 1, 5, '30000', 14, '1'),
+(7, 'cristianoronaldoisthegoat', 'CR', 6, 6, '5000000', 0, '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satuan_produk`
+--
+
+CREATE TABLE `satuan_produk` (
+  `id` int(11) NOT NULL,
+  `satuan` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `satuan_produk`
+--
+
+INSERT INTO `satuan_produk` (`id`, `satuan`) VALUES
+(1, 'Pack'),
+(2, 'Karton'),
+(3, 'Lusin'),
+(4, 'Buah'),
+(5, 'Dus'),
+(6, 'Orang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok_keluar`
+--
+
+CREATE TABLE `stok_keluar` (
+  `id` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `barcode` int(11) NOT NULL,
+  `jumlah` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Keterangan` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `stok_keluar`
+--
+
+INSERT INTO `stok_keluar` (`id`, `tanggal`, `barcode`, `jumlah`, `Keterangan`) VALUES
+(1, '2022-05-12 14:56:28', 3, '3', 'hilang'),
+(2, '2022-05-12 15:06:50', 4, '10', 'kadaluarsa'),
+(3, '2022-05-12 15:20:30', 5, '2', 'hilang'),
+(4, '2022-05-12 15:46:09', 6, '5', 'kadaluarsa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok_masuk`
+--
+
+CREATE TABLE `stok_masuk` (
+  `id` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `barcode` int(11) NOT NULL,
+  `jumlah` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keterangan` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `supplier` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `stok_masuk`
+--
+
+INSERT INTO `stok_masuk` (`id`, `tanggal`, `barcode`, `jumlah`, `keterangan`, `supplier`) VALUES
+(1, '2022-05-12 14:56:05', 3, '100', 'penambahan', 1),
+(2, '2022-05-12 14:05:08', 4, '50', 'penambahan', 6),
+(3, '2022-05-11 15:35:38', 5, '12', 'penambahan', 1),
+(4, '2022-05-11 14:00:20', 6, '20', 'penambahan', 7),
+(5, '2023-07-30 19:45:03', 7, '1', 'penambahan', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telepon` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keterangan` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `nama`, `alamat`, `telepon`, `keterangan`) VALUES
+(1, 'Surya Abadi Tbk.', 'Surabaya', '088250089674', 'PT SIDU Tangerang'),
+(7, 'Mutiara Bogor', 'Bogor', '900', 'pT. CHUBA SEJAHTERA'),
+(9, 'Revan Naufal Arifin', 'London', '9102992019', 'fans cristiano');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `toko`
+--
+
+CREATE TABLE `toko` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `toko`
+--
+
+INSERT INTO `toko` (`id`, `nama`, `alamat`) VALUES
+(1, 'Toko Ihsan', 'Kab. Bandung, Jawa barat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `barcode` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qty` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_bayar` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah_uang` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `diskon` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pelanggan` int(11) DEFAULT NULL,
+  `nota` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kasir` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `tanggal`, `barcode`, `qty`, `total_bayar`, `jumlah_uang`, `diskon`, `pelanggan`, `nota`, `kasir`) VALUES
+(3, '2022-05-12 15:12:19', '4,3', '1,1', '74000', '100000', '', 2, 'RHOJGRXOZGV190N', 1),
+(4, '2022-05-12 15:12:48', '4,3', '1,1', '74000', '100000', '', 2, 'T3FWAF3FJ7JF6EY', 1),
+(6, '2022-05-12 15:47:19', '6,3', '9,1', '246000', '250000', '', 1, 'MEBBHX94BY17M0X', 1),
+(7, '2023-07-30 19:45:48', '7', '1', '5000000', '10000000', '2000000', 5, '7UNWL6ZFH3O34ZU', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kategori_produk`
+--
+ALTER TABLE `kategori_produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `satuan_produk`
+--
+ALTER TABLE `satuan_produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stok_keluar`
+--
+ALTER TABLE `stok_keluar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stok_masuk`
+--
+ALTER TABLE `stok_masuk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `toko`
+--
+ALTER TABLE `toko`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kategori_produk`
+--
+ALTER TABLE `kategori_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `satuan_produk`
+--
+ALTER TABLE `satuan_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `stok_keluar`
+--
+ALTER TABLE `stok_keluar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `stok_masuk`
+--
+ALTER TABLE `stok_masuk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `toko`
+--
+ALTER TABLE `toko`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
